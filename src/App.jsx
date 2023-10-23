33cbdb84f79b7c0d638a8ff8e98adb81edd40e0b
VFdo <vidufernando98@gmail.com>
2023-10-23 09:37:30 +0530

    vidu changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './components/about'
 import Shop from './components/shop'
 import Services from './components/services'
 import Contact from './components/contact'
+import ItemPage from './pages/ItemPage'
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
         <Route path='/' element={<Layout/>}>
         <Route path='about' element={<About/>}/>
         <Route path='shop' element={<Shop/>}/>
-        <Route path='services' element={<Services/>}/>
+        <Route path='services' element={<ItemPage/>}/>
         <Route path='contact' element={<Contact/>}/>
           
         </Route>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import TableComp from './components/TableComp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <TableComp />
   </React.StrictMode>,
 )
