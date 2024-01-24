6fb4d568bf17ce03879894fd2d2c4ba55ad3a847
VFdo <vidufernando98@gmail.com>
2024-01-24 09:55:54 +0530

    getting all pets to table

--- src/pages/pet/PetHistory.jsx
+++ src/pages/pet/PetHistory.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState , useEffect} from "react";
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -7,59 +7,51 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import Grid from '@mui/material/Grid';
+
+const styles = {
+    tableContainer: {
+        margin: 16,
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center', 
+      },
+}
 
 const columns = [
   { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
-];
+  { id: 'dob', label: 'Date of Birth', minWidth: 100 },
+  { id: 'typeOfAnimal', label: 'Type of Animal', minWidth: 100 },
+  { id: 'photo', label: 'Photo', minWidth: 100 },
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+  // ***** FOR FUTURE REFERENCE *****
+  // {
+  //   id: 'population',
+  //   label: 'Population',
+  //   minWidth: 170,
+  //   align: 'right',
+  //   format: (value) => value.toLocaleString('en-US'),
+  // },
+  // {
+  //   id: 'size',
+  //   label: 'Size\u00a0(km\u00b2)',
+  //   minWidth: 170,
+  //   align: 'right',
+  //   format: (value) => value.toLocaleString('en-US'),
+  // },
+  // {
+  //   id: 'density',
+  //   label: 'Density',
+  //   minWidth: 170,
+  //   align: 'right',
+  //   format: (value) => value.toFixed(2),
+  // },
 ];
 
 const PetHistory = () => {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [rows, setData] = useState([]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -70,55 +62,76 @@ const PetHistory = () => {
         setPage(0);
     };
 
+    useEffect(() => {
+      const fetchAllPets = async () => {
+        try{
+          console.log('Fetching data...');
+          const res = await fetch('http://localhost:8080/pet');
+          if (!res.ok) {
+            throw new Error('Failed to fetch pets');
+          }
+          const petData = await res.json();
+          console.log('data: ', petData);
+          setData(petData);
+        }catch (error) {
+          console.error('Error fetching pets:', error);
+        }
+      };
+      fetchAllPets();
+    }, []);
+    
+
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} style={styles.tableContainer}>
+        <Paper sx={{ width: '80%', overflow: 'hidden' }}>
+        <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+                <TableRow>
+                {columns.map((column) => (
+                    <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                    >
+                    {column.label}
+                    </TableCell>
+                ))}
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {rows
+                // .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                    return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                        {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                            <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === 'number'
+                                ? column.format(value)
+                                : value}
+                            </TableCell>
+                        );
+                        })}
+                    </TableRow>
+                    );
+                })}
+            </TableBody>
+            </Table>
+        </TableContainer>
+        <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+        </Paper>
+    </Grid>
     );
 };
 
