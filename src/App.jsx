1f3ec59b29e9cd0f28fb93d2e41c858fcf58ed58
savindi99 <induje1999@gmail.com>
2024-02-06 12:35:12 +0530

    Employee UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
+import "./App.css";
+import Layout from "./components/layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/about";
+import Shop from "./components/shop";
+import Services from "./components/services";
+import Contact from "./components/contact";
+import ItemPage from "./pages/ItemPage";
+import PetPage from "./pages/pet/PetPage";
+import PetHistory from "./pages/pet/PetHistory";
+import PetDetailsForm from "./pages/pet/PetDetailsForm";
+import EmployeeDetailsForm from "./pages/employee/EmployeeDetailsForm";
+import AdminView from "./pages/employee/AdminView";
+import EmployeePage from "./pages/employee/EmployeePage";
+import AddEmployee from "./pages/employee/AddEmployee";
 
-import './App.css'
-import Layout from './components/layout'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import About from './components/about'
-import Shop from './components/shop'
-import Services from './components/services'
-import Contact from './components/contact'
-import ItemPage from './pages/ItemPage'
-import PetPage from './pages/pet/PetPage'
-import PetHistory from './pages/pet/PetHistory'
-
-
-function App() {
-  
-  return (
+function App() { return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-        <Route path='about' element={<About/>}/>
-        <Route path='shop' element={<Shop/>}/>
-        {/* <Route path='services' element={<PetPage titleText = "test" descriptionText = "desc" buttonLeft = "view" buttonRight = "history"/>}/> */}
-        <Route path='services' element={<PetHistory/>}/>
-        <Route path='contact' element={<Contact/>}/>
-          
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="about" element={<About />} />
+            <Route path="shop" element={<Shop />} />
+            {/* <Route path='services' element={<PetPage titleText = "test" descriptionText = "desc" buttonLeft = "view" buttonRight = "history"/>}/> */}
+            <Route path="services" element={<AddEmployee />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
